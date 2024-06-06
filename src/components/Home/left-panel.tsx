8c375ffcd4075e976fabd7ed839a86f14cd54d02
Shivam00123 <shivamjackson6@gmail.com>
2024-06-06 18:30:08 +0530

    added clerk webhook for authentication

--- src/components/Home/left-panel.tsx
+++ src/components/Home/left-panel.tsx
@@ -9,6 +9,7 @@ import { Input } from "../ui/input";
 import ThemeSwitch from "./theme-switch";
 import { conversations } from "@/app/dummy-data/db";
 import Conversation from "./conversation";
+import { UserButton } from "@clerk/nextjs";
 
 const LeftPanel = () => {
   // const conversations = [];
@@ -18,8 +19,8 @@ const LeftPanel = () => {
       <div className="sticky top-0 bg-left-panel z-10">
         {/* Header */}
         <div className="flex justify-between bg-gray-primary p-3 items-center">
-          <User size={24} />
-
+          {/* <User size={24} /> */}
+          <UserButton />
           <div className="flex items-center gap-3">
             <MessageSquareDiff size={20} />{" "}
             {/* TODO: This line will be replaced with <UserListDialog /> */}

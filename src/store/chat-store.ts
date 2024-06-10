34a740c767b198c4b6bc31775e29b074ad145330
Shivam00123 <shivamjackson6@gmail.com>
2024-06-10 23:49:23 +0530

    fixed issue

--- src/store/chat-store.ts
+++ src/store/chat-store.ts
@@ -9,7 +9,7 @@ export type Conversation = {
   name?: string;
   groupName?: string;
   groupImage?: string;
-  isOnline: boolean;
+  isOnline?: boolean;
   admin?: Id<"users">;
   lastMessage?: {
     _id: Id<"messages">;
@@ -40,5 +40,5 @@ export interface IMessage {
   senderName: string;
   senderImage: string;
   senderIsOnline: boolean;
-  isOnline: boolean;
+  isOnline?: boolean;
 }
